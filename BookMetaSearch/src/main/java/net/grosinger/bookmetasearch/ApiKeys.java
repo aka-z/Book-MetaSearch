7b9e751ec4914336c693e86e94c68bb468e22652
Tony Grosinger <tony@grosinger.net>
2013-11-03 04:56:56 -0800

    Added ApiKey storage location

---
+++ BookMetaSearch/src/main/java/net/grosinger/bookmetasearch/ApiKeys.java
@@ -0,0 +1,9 @@
+package net.grosinger.bookmetasearch;
+
+/**
+ * Created by tony on 11/3/13.
+ */
+public class ApiKeys {
+    // This class should be populated with any API keys you wish to use.
+    // I am not going to give you mine.
+}
